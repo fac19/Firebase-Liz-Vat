58494e6272af43ae6bd22f7942762e7cb056f78b
Vatsal Kanoria <vatskan@gmail.com>
2020-06-22 16:32:16 +0100

    try to add auth function
    Co-authored-by: lizzy-j lizzyhj@hotmail.com

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { auth } from 'firebase';
+
+
 function App() {
-  // const [name, setName] = React.useState({});  
-  const [data, setData] = React.useState({});
+  // const [data, setData] = React.useState({});
 
   const dataObj = {};
 
@@ -16,12 +18,19 @@ function App() {
   }
 
   const handleSubmission = () => {
-    setData(dataObj)
+    // setData(dataObj)
+    auth().signInWithEmailAndPassword(dataObj.email, dataObj.password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("you have not logged in successfully")
+      console.log(errorCode, errorMessage);
+    });
   }
 
-  React.useEffect(()=>{
-    console.log(data)
-  }, [data])
+  // React.useEffect(()=>{
+  //   console.log(data)
+  // }, [data])
 
   return (
     <form>
